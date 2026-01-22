846a34c34675229f1afee1fd5a0ded3a7e8bab9c
pujandhakal <dhakalpujan72@gmail.com>
2026-01-22 22:35:08 +0545

    Ongoing Login Screen

--- yatritech/lib/screens/user/login_in_screen.dart
+++ yatritech/lib/screens/user/login_in_screen.dart
@@ -23,6 +23,40 @@ class _LoginInScreenState extends State<LoginInScreen> {
             "Sign in to your safety platform",
             style: TextStyle(fontSize: 16, color: Color(0xff6C757D)),
           ),
+
+          SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(28),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Color.fromARGB(50, 0, 0, 0),
+                  offset: Offset(0, 8),
+                  blurRadius: 32,
+                  spreadRadius: 0,
+                ),
+                BoxShadow(
+                  color: Color.fromARGB(40, 0, 0, 0),
+                  offset: Offset(0, 2),
+                  blurRadius: 8,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Text(
+                  "Email Address",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xff343A40),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
