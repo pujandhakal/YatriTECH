2fdfd7f5621894e00bd4f5586bd3156d32401db4
pujandhakal <dhakalpujan72@gmail.com>
2026-04-19 22:24:53 +0545

    Added svg

--- yatritech/lib/screens/user/onboarding/onboarding_first_screen.dart
+++ yatritech/lib/screens/user/onboarding/onboarding_first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardingFirstScreen extends StatelessWidget {
   const OnboardingFirstScreen({super.key});
@@ -6,7 +7,12 @@ class OnboardingFirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(padding: EdgeInsets.symmetric(horizontal: 16)),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(children: [
+        SvgPicture.asset('assets/rafiki.svg'),
+      ],),
+      ),
     );
   }
 }
